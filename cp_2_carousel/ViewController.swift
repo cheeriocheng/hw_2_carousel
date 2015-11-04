7c916458a770a2af1757c40f99e42326e33fbf54
cheng <gnuchex@gmail.com>
2015-11-04 10:14:24 -0800

    Initial Commit

---
+++ cp_2_carousel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cp_2_carousel
+//
+//  Created by Xu, Cheng on 11/4/15.
+//  Copyright © 2015 sansserif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
