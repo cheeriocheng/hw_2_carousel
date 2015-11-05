f1f93320f7f8367b64327dd9c4beae005e1c6d19
cheng <gnuchex@gmail.com>
2015-11-04 22:52:15 -0800

    creditial check and alerts

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -14,11 +14,17 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var fieldView: UIView!
     @IBOutlet weak var buttonView: UIView!
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     
-        var fieldInitialY: CGFloat!
-        var buttonInitialY: CGFloat!
-        var fieldDeltaY: CGFloat!
-        var buttonDeltaY:CGFloat!
+    @IBOutlet weak var logInButton: UIButton!
+    
+    @IBOutlet weak var loadingActivityIndicatorView: UIActivityIndicatorView!
+    
+    var fieldInitialY: CGFloat!
+    var buttonInitialY: CGFloat!
+    var fieldDeltaY: CGFloat!
+    var buttonDeltaY:CGFloat!
     
     
     override func viewDidLoad() {
@@ -39,6 +45,40 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func onLogInButtonPressed(sender: AnyObject) {
+//        print("sign in button pressed")
+        
+        //check if fields are empty, if so, alert
+        if (emailTextField.text!.isEmpty  ){
+            emptyFieldAlert("email")
+        }
+ 
+        else if ( passwordTextField.text!.isEmpty) {
+             emptyFieldAlert("password")
+        }
+        
+        //else send the information to server
+        //(i.e. delay )
+        else{
+            loadingActivityIndicatorView.startAnimating()
+            logInButton.enabled=false
+            view.userInteractionEnabled = false
+            logInButton.selected = true
+            
+            delay(2) { () -> () in
+                self.validateCredentials()
+                self.loadingActivityIndicatorView.stopAnimating()
+                self.view.userInteractionEnabled = true
+                self.logInButton.enabled = true
+                //self.loginButton.selected = false
+            }
+
+            
+            
+        }
+        
+        
+    }
     
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
@@ -49,6 +89,45 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func validateCredentials(){
+        if emailTextField.text == "tim" && passwordTextField.text == "password" {
+            print("bingo")
+            // performSegueWithIdentifier("loginSegue", sender: nil)
+            
+        } else {
+            print("invalid credentials")
+            
+            let alert = UIAlertController(title: "Oops", message: "Wrong password sucka", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+                print("User tapped OK")
+            })
+            
+            alert.addAction(okAction)
+            
+            presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func emptyFieldAlert(missingString: String){
+        let temp = "Please enter your " + missingString
+        let alert = UIAlertController(title: "Try again", message: temp, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+            print("User tapped OK")
+        })
+        
+        alert.addAction(okAction)
+//
+//        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+//            print("User tapped cancel")
+//        })
+//        
+//        alert.addAction(cancelAction)
+        
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
     func keyboardWillShow(notification: NSNotification!) {
         
         fieldView.frame.origin.y = fieldInitialY + fieldDeltaY
